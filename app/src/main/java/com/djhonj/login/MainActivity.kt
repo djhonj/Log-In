4bc2f59d77f9b8d7c33ecea5f13de88676b31f34
Jhon Diaz <diazj691@gmail.com>
2021-06-16 20:28:56 -0500

    terminada vista registro. se agregaron values dimens y strings

--- app/src/main/java/com/djhonj/login/MainActivity.kt
+++ app/src/main/java/com/djhonj/login/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.djhonj.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.djhonj.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSingup.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
